c5d88e155f1fd964444d5b25075f270a6c3c7a8f
rinsoo <rinsoo1994@gmail.com>
2022-04-21 10:33:50 +0900

    Add: JPA Repository

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,23 +1,23 @@
 package com.example.hellospring;
 
-import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
-import com.example.hellospring.repository.jdbcMemberRepository;
-import com.example.hellospring.repository.jdbcTemplateMemberRepository;
+import com.example.hellospring.repository.*;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
+    private final EntityManager entityManagerm;
+
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager entityManagerm) {
+        this.entityManagerm = entityManagerm;
     }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -25,6 +25,6 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
-        return new jdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(entityManagerm);
     }
 }
--- src/main/java/com/example/hellospring/domain/Member.java
+++ src/main/java/com/example/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package com.example.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public String getName() {
